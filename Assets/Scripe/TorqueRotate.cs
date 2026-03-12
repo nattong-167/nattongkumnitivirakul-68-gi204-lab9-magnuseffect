9bc4adb4750312525448c4df2af04863b0e2b821
nattong-167 <nat.tong@bumail.net>
2026-03-12 21:56:08 +0700

    LAB9 FInal

--- Assets/Scripe/TorqueRotate.cs
+++ Assets/Scripe/TorqueRotate.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TorqueRotate : MonoBehaviour
 {
@@ -12,6 +13,10 @@ public class TorqueRotate : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.AddTorque(new Vector3(0, TorquePower, 0));
+        if (Keyboard.current.dKey.isPressed)
+        {
+            rb.AddTorque(new Vector3(0, TorquePower, 0));
+        }
+        
     }
 }
